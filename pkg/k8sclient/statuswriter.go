991f97e3f2a6cd01576257f2ea4bbf40c468b8ff
Ron Green <11993626+georgettica@users.noreply.github.com>
2020-06-10 17:22:45 +0300

    fixup! refactor(k8sclient): seperate classes into different files

--- pkg/k8sclient/statuswriter.go
+++ pkg/k8sclient/statuswriter.go
@@ -1,3 +1,3 @@
 package k8sclient
 
-//go:generate mockgen -destination=../util/mocks/$GOPACKAGE/statuswriter.go -package=$GOPACKAGE sigs.k8s.io/controller-runtime/pkg/client StatusWriter
+//go:generate mockgen -destination=../util/mocks/$GOPACKAGE/$GOFILE -package=$GOPACKAGE sigs.k8s.io/controller-runtime/pkg/client StatusWriter
