8b3836d9c013f20488fb8c42eebf9cd8bcd9f4a9
Ron Green <11993626+georgettica@users.noreply.github.com>
2020-09-02 17:55:11 +0300

    feat(ProjectClaim): add controller
    * remove reference to old library

---
+++ controllers/projectclaim_adapter.go
@@ -0,0 +1,293 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-logr/logr"
+
+	gcpv1alpha1 "github.com/openshift/gcp-project-operator/api/v1alpha1"
+	condition "github.com/openshift/gcp-project-operator/pkg/condition"
+	gcputil "github.com/openshift/gcp-project-operator/pkg/util"
+	operrors "github.com/openshift/gcp-project-operator/pkg/util/errors"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type ProjectClaimAdapter struct {
+	projectClaim     *gcpv1alpha1.ProjectClaim
+	logger           logr.Logger
+	client           client.Client
+	projectReference *gcpv1alpha1.ProjectReference
+	conditionManager condition.Conditions
+}
+
+type ObjectState bool
+
+const (
+	ObjectModified  ObjectState = true
+	ObjectUnchanged ObjectState = false
+)
+
+const ProjectClaimFinalizer string = "finalizer.gcp.managed.openshift.io"
+
+// Regions supported in the gcrg p-project-operator
+var supportedRegions = map[string]bool{
+	"asia-east1":      true,
+	"asia-northeast1": true,
+	"asia-southeast1": true,
+	"europe-west1":    true,
+	"europe-west4":    true,
+	"us-central1":     true,
+	"us-east1":        true,
+	"us-east4":        true,
+	"us-west1":        true,
+
+	// Regions below don't have enough quota configured by default, but our org has sufficient quota
+	"asia-east2":   true,
+	"asia-south1":  true,
+	"europe-west2": true,
+	"us-west2":     true,
+
+	// Regions below have enough quota, but the region name will produce too long hostnames.
+	// This is an issue the openshift installer needs to fix.
+	// "australia-southeast1":    true,
+	// "northamerica-northeast1": true,
+	// "southamerica-east1":      true,
+
+	// Regions below are disabled do not have enough quota configured (CPU < 28 or SSD storage < 896)
+	// "europe-west3":            true,
+	// "europe-west6":            true,
+	// "europe-north1":           true,
+	// "asia-northeast2":         true,
+}
+
+func NewProjectClaimAdapter(projectClaim *gcpv1alpha1.ProjectClaim, logger logr.Logger, client client.Client, manager condition.Conditions) *ProjectClaimAdapter {
+	projectReference := newMatchingProjectReference(projectClaim)
+	return &ProjectClaimAdapter{projectClaim, logger, client, projectReference, manager}
+}
+
+func newMatchingProjectReference(projectClaim *gcpv1alpha1.ProjectClaim) *gcpv1alpha1.ProjectReference {
+
+	return &gcpv1alpha1.ProjectReference{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      projectClaim.GetNamespace() + "-" + projectClaim.GetName(),
+			Namespace: gcpv1alpha1.ProjectReferenceNamespace,
+		},
+		Spec: gcpv1alpha1.ProjectReferenceSpec{
+			GCPProjectID: "",
+			ProjectClaimCRLink: gcpv1alpha1.NamespacedName{
+				Name:      projectClaim.GetName(),
+				Namespace: projectClaim.GetNamespace(),
+			},
+			LegalEntity: *projectClaim.Spec.LegalEntity.DeepCopy(),
+		},
+	}
+}
+
+func (c *ProjectClaimAdapter) ProjectReferenceExists() (bool, error) {
+	found := &gcpv1alpha1.ProjectReference{}
+	err := c.client.Get(context.TODO(), types.NamespacedName{Name: c.projectReference.Name, Namespace: c.projectReference.Namespace}, found)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func (adapter *ProjectClaimAdapter) EnsureProjectClaimDeletionProcessed() (gcputil.OperationResult, error) {
+	if adapter.IsProjectClaimDeletion() {
+		crState, err := adapter.FinalizeProjectClaim()
+		if crState == ObjectUnchanged || err != nil {
+			return gcputil.RequeueAfter(5*time.Second, err)
+		}
+		return gcputil.StopProcessing()
+	}
+	return gcputil.ContinueProcessing()
+}
+
+func (c *ProjectClaimAdapter) IsProjectClaimDeletion() bool {
+	return c.projectClaim.DeletionTimestamp != nil
+}
+
+func (c *ProjectClaimAdapter) IsProjectReferenceDeletion() bool {
+	return c.projectReference.DeletionTimestamp != nil
+}
+
+func (c *ProjectClaimAdapter) EnsureFinalizerDeleted() error {
+	c.logger.Info("Deleting ProjectClaim Finalizer")
+	finalizers := c.projectClaim.GetFinalizers()
+	if gcputil.Contains(finalizers, ProjectClaimFinalizer) {
+		c.projectClaim.SetFinalizers(gcputil.Filter(finalizers, ProjectClaimFinalizer))
+		return c.client.Update(context.TODO(), c.projectClaim)
+	}
+	return nil
+}
+
+func (c *ProjectClaimAdapter) FinalizeProjectClaim() (ObjectState, error) {
+	projectReferenceExists, err := c.ProjectReferenceExists()
+	if err != nil {
+		return ObjectUnchanged, err
+	}
+
+	projectReferenceDeletionRequested := c.IsProjectReferenceDeletion()
+	if projectReferenceExists && !projectReferenceDeletionRequested {
+		err := c.client.Delete(context.TODO(), c.projectReference)
+		if err != nil {
+			return ObjectUnchanged, err
+		}
+	}
+
+	// Assure the finalizer is not deleted as long as ProjectReference exists
+	if !projectReferenceExists {
+		err := c.EnsureFinalizerDeleted()
+		if err != nil {
+			return ObjectUnchanged, err
+		}
+		return ObjectModified, nil
+	}
+
+	return ObjectUnchanged, nil
+}
+
+func (c *ProjectClaimAdapter) EnsureProjectClaimInitialized() (gcputil.OperationResult, error) {
+	if c.projectClaim.Status.Conditions == nil {
+		c.projectClaim.Status.Conditions = []gcpv1alpha1.Condition{}
+		err := c.client.Status().Update(context.TODO(), c.projectClaim)
+		if err != nil {
+			gcputil.RequeueWithError(operrors.Wrap(err, "failed to initalize projectclaim"))
+		}
+		return gcputil.StopProcessing()
+	}
+	return gcputil.ContinueProcessing()
+}
+
+func (c *ProjectClaimAdapter) EnsureProjectReferenceLink() (gcputil.OperationResult, error) {
+	expectedLink := gcpv1alpha1.NamespacedName{
+		Name:      c.projectReference.GetName(),
+		Namespace: c.projectReference.GetNamespace(),
+	}
+	if c.projectClaim.Spec.ProjectReferenceCRLink == expectedLink {
+		return gcputil.ContinueProcessing()
+	}
+	c.projectClaim.Spec.ProjectReferenceCRLink = expectedLink
+	err := c.client.Update(context.TODO(), c.projectClaim)
+	if err != nil {
+		return gcputil.RequeueWithError(err)
+	}
+	return gcputil.StopProcessing()
+}
+
+func (c *ProjectClaimAdapter) EnsureFinalizer() (gcputil.OperationResult, error) {
+	if !gcputil.Contains(c.projectClaim.GetFinalizers(), ProjectClaimFinalizer) {
+		c.logger.Info("Adding Finalizer to the ProjectClaim")
+		c.projectClaim.SetFinalizers(append(c.projectClaim.GetFinalizers(), ProjectClaimFinalizer))
+
+		err := c.client.Update(context.TODO(), c.projectClaim)
+		if err != nil {
+			return gcputil.RequeueWithError(operrors.Wrap(err, "failed to initalize projectclaim with finalizer"))
+		}
+		return gcputil.StopProcessing()
+	}
+	return gcputil.ContinueProcessing()
+}
+
+func (c *ProjectClaimAdapter) EnsureProjectReferenceExists() (gcputil.OperationResult, error) {
+	projectReferenceExists, err := c.ProjectReferenceExists()
+	if err != nil {
+		return gcputil.RequeueWithError(err)
+	}
+
+	if !projectReferenceExists {
+		return gcputil.RequeueOnErrorOrContinue(c.client.Create(context.TODO(), c.projectReference))
+	}
+	return gcputil.ContinueProcessing()
+}
+
+func (c *ProjectClaimAdapter) EnsureProjectClaimStatePending() (gcputil.OperationResult, error) {
+	return c.EnsureProjectClaimState(gcpv1alpha1.ClaimStatusPending)
+}
+
+func (c *ProjectClaimAdapter) EnsureProjectClaimStatePendingProject() (gcputil.OperationResult, error) {
+	return c.EnsureProjectClaimState(gcpv1alpha1.ClaimStatusPendingProject)
+}
+
+func (c *ProjectClaimAdapter) EnsureProjectClaimState(state gcpv1alpha1.ClaimStatus) (gcputil.OperationResult, error) {
+	if c.projectClaim.Status.State == state {
+		return gcputil.ContinueProcessing()
+	}
+
+	if state == gcpv1alpha1.ClaimStatusPending && c.projectClaim.Status.State != gcpv1alpha1.ClaimStatusError {
+		if c.projectClaim.Status.State != "" {
+			return gcputil.ContinueProcessing()
+		}
+	}
+
+	if state == gcpv1alpha1.ClaimStatusPendingProject {
+		if c.projectClaim.Status.State != gcpv1alpha1.ClaimStatusPending {
+			return gcputil.ContinueProcessing()
+		}
+	}
+
+	c.projectClaim.Status.State = state
+	err := c.StatusUpdate()
+	if err != nil {
+		return gcputil.RequeueWithError(err)
+	}
+
+	return gcputil.StopProcessing()
+}
+
+// SetProjectClaimCondition calls SetCondition() with project claim conditions
+func (c *ProjectClaimAdapter) SetProjectClaimCondition(reason string, err error) error {
+	conditions := &c.projectClaim.Status.Conditions
+	conditionType := gcpv1alpha1.ConditionError
+	if err != nil {
+		c.conditionManager.SetCondition(conditions, conditionType, corev1.ConditionTrue, reason, err.Error())
+	} else {
+		if len(*conditions) != 0 {
+			reason = reason + "Resolved"
+			c.conditionManager.SetCondition(conditions, conditionType, corev1.ConditionFalse, reason, "")
+		} else {
+			return nil
+		}
+	}
+
+	return c.StatusUpdate()
+}
+
+// IsRegionSupported checks if current region is supported.
+// It returns an error message if a region is not supported.
+func (c *ProjectClaimAdapter) IsRegionSupported() error {
+	if _, ok := supportedRegions[c.projectClaim.Spec.Region]; !ok {
+		return operrors.ErrRegionNotSupported
+	}
+	return nil
+}
+
+// EnsureRegionSupported modifies projectClaim.Status.State with result from IsRegionSupported.
+// If a region is not supported it returns an error and sets projectClaim.Status.State to ClaimStatusError.
+func (c *ProjectClaimAdapter) EnsureRegionSupported() (gcputil.OperationResult, error) {
+	if err := c.IsRegionSupported(); err != nil {
+		c.projectClaim.Status.State = gcpv1alpha1.ClaimStatusError
+		c.StatusUpdate()
+		return gcputil.RequeueWithError(operrors.Wrap(err, ""))
+	}
+	return gcputil.ContinueProcessing()
+}
+
+// StatusUpdate updates the project claim status
+func (c *ProjectClaimAdapter) StatusUpdate() error {
+	if err := c.client.Status().Update(context.TODO(), c.projectClaim); err != nil {
+		return operrors.Wrap(err, fmt.Sprintf("failed to update ProjectClaim state for %s", c.projectClaim.Name))
+	}
+
+	return nil
+}
--- controllers/projectclaim_controller.go
+++ controllers/projectclaim_controller.go
@@ -18,13 +18,19 @@ package controllers
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	gcpmanagedopenshiftiov1alpha1 "github.com/openshift/gcp-project-operator/api/v1alpha1"
+	"github.com/openshift/gcp-project-operator/pkg/condition"
+	"github.com/openshift/gcp-project-operator/pkg/util"
+	gcputil "github.com/openshift/gcp-project-operator/pkg/util"
 )
 
 // ProjectClaimReconciler reconciles a ProjectClaim object
@@ -34,20 +40,94 @@ type ProjectClaimReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+//go:generate mockgen -destination=../../util/mocks/$GOPACKAGE/customeresourceadapter.go -package=$GOPACKAGE github.com/openshift/gcp-project-operator/pkg/controller/projectclaim CustomResourceAdapter
+type CustomResourceAdapter interface {
+	EnsureProjectClaimDeletionProcessed() (gcputil.OperationResult, error)
+	ProjectReferenceExists() (bool, error)
+	EnsureProjectClaimInitialized() (gcputil.OperationResult, error)
+	EnsureProjectClaimStatePending() (gcputil.OperationResult, error)
+	EnsureProjectClaimStatePendingProject() (gcputil.OperationResult, error)
+	EnsureRegionSupported() (gcputil.OperationResult, error)
+	EnsureProjectReferenceExists() (gcputil.OperationResult, error)
+	EnsureProjectReferenceLink() (gcputil.OperationResult, error)
+	EnsureFinalizer() (gcputil.OperationResult, error)
+	FinalizeProjectClaim() (ObjectState, error)
+}
+
 // +kubebuilder:rbac:groups=gcp.managed.openshift.io.my.domain,resources=projectclaims,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=gcp.managed.openshift.io.my.domain,resources=projectclaims/status,verbs=get;update;patch
-
 func (r *ProjectClaimReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	_ = context.Background()
-	_ = r.Log.WithValues("projectclaim", req.NamespacedName)
+	log := r.Log.WithValues("projectclaim", req.NamespacedName)
 
-	// your logic here
+	reqLogger := log.WithValues("Request.Namespace", req.Namespace, "Request.Name", req.Name)
 
-	return ctrl.Result{}, nil
+	// Fetch the ProjectClaim instance
+	instance := &gcpmanagedopenshiftiov1alpha1.ProjectClaim{}
+	err := r.Client.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			return r.doNotRequeue()
+		}
+		return r.requeueOnErr(err)
+	}
+
+	conditionManager := condition.NewConditionManager()
+	adapter := NewProjectClaimAdapter(instance, reqLogger, r.Client, conditionManager)
+	result, err := r.ReconcileHandler(adapter)
+	reason := "ReconcileError"
+	_ = adapter.SetProjectClaimCondition(reason, err)
+
+	return result, err
+
+}
+
+type ReconcileOperation func() (util.OperationResult, error)
+
+// ReconcileHandler reads that state of the cluster for a ProjectClaim object and makes changes based on the state read
+// and what is in the ProjectClaim.Spec
+// Note:
+// The Controller will requeue the Request to be processed again if the returned error is non-nil or
+// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
+func (r *ProjectClaimReconciler) ReconcileHandler(adapter CustomResourceAdapter) (reconcile.Result, error) {
+	operations := []ReconcileOperation{
+		adapter.EnsureProjectClaimDeletionProcessed,
+		adapter.EnsureProjectClaimInitialized,
+		adapter.EnsureRegionSupported,
+		adapter.EnsureProjectClaimStatePending,
+		adapter.EnsureProjectReferenceExists,
+		adapter.EnsureProjectReferenceLink,
+		adapter.EnsureFinalizer,
+		adapter.EnsureProjectClaimStatePendingProject,
+	}
+	for _, operation := range operations {
+		result, err := operation()
+		if err != nil || result.RequeueRequest {
+			return r.requeueAfter(result.RequeueDelay, err)
+		}
+		if result.CancelRequest {
+			return r.doNotRequeue()
+		}
+	}
+	return r.doNotRequeue()
 }
 
 func (r *ProjectClaimReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&gcpmanagedopenshiftiov1alpha1.ProjectClaim{}).
 		Complete(r)
+}
+
+func (r *ProjectClaimReconciler) doNotRequeue() (ctrl.Result, error) {
+	return ctrl.Result{}, nil
+}
+
+func (r *ProjectClaimReconciler) requeueOnErr(err error) (ctrl.Result, error) {
+	return ctrl.Result{}, err
+}
+
+func (r *ProjectClaimReconciler) requeueAfter(duration time.Duration, err error) (ctrl.Result, error) {
+	return ctrl.Result{RequeueAfter: duration}, err
 }
